0a91561fd9ea7a73000cc92ef27309671b237a7f
okatechnology <tonkotsu6218@gmail.com>
2019-11-24 15:02:54 +0900

    added import html files

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,13 @@
 import '../html/index.html';
+import '../html/bmw.html';
+import '../html/emblem.html';
+import '../html/features.html';
+import '../html/motor-sports.html';
+import '../html/motorcycle.html';
+import '../html/business-conditions.html';
+
 import '../scss/index.scss';
+
 import './parallax';
 import './menuButton';
 import './scrollButton';
