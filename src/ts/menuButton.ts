796f2c855b8b00df672e0fe66edb42701555ce7a
okatechnology <tonkotsu6218@gmail.com>
2019-11-30 07:18:47 +0900

    change: refactoring opening menu function

--- src/ts/menuButton.ts
+++ src/ts/menuButton.ts
@@ -1,43 +1,23 @@
 {
-  const menuButton = document.querySelectorAll<HTMLElement>('.menu__button');
-  const menuLinks = document.querySelectorAll<HTMLAnchorElement>(
-    '.menuList__item__anchor',
-  );
-  const allMenuItems = document.querySelectorAll<HTMLElement>('.menu, .menu *');
-  let shouldCloseMenu = true;
+  const menuButton = document.querySelector<HTMLElement>('.menu__button');
+  const menuList = document.querySelector<HTMLElement>('.menuList');
 
-  const toggleActive = (targetNodeList: NodeListOf<HTMLElement>) => {
-    return () => {
-      targetNodeList.forEach((elem) => {
-        elem.classList.toggle(`${elem.classList.item(0)}--active`);
-      });
-    };
+  const toggleOpening = () => {
+    menuList?.classList.toggle('menuList--active');
+    menuButton?.classList.toggle('menu__button--active');
+    if (menuList?.classList.contains('menuList--active')) return;
+    menuList?.classList.add('menuList--closing');
   };
+  menuButton?.addEventListener('click', toggleOpening);
 
-  menuButton.forEach((menuButtonElem) => {
-    menuButtonElem.addEventListener('click', toggleActive(allMenuItems));
-  });
-
-  menuLinks.forEach((elem) => {
-    elem.addEventListener('click', (e) => {
-      const invaild = !elem.classList.contains('menuList__item__anchor--active');
-      if (invaild) {
-        e.preventDefault();
-      }
-    });
+  menuList?.addEventListener('animationend', () => {
+    if (!menuList.classList.contains('menuList--closing')) return;
+    menuList.classList.remove('menuList--closing');
   });
 
   window.addEventListener('click', (e) => {
-    allMenuItems.forEach((elem) => {
-      if (elem === e.target) {
-        shouldCloseMenu = false;
-      }
-    });
-    if (shouldCloseMenu) {
-      allMenuItems.forEach((elem) => {
-        elem.classList.remove(`${elem.classList[0]}--active`);
-      });
-    }
-    shouldCloseMenu = true;
+    if (!menuList?.classList.contains('menuList--active')) return;
+    if (e.target === menuButton || e.target === menuList) return;
+    toggleOpening();
   });
 }
